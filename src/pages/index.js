cce19dd2bd1ddb75308e0bed0a200545e8ee369e
Deepak Prabhakara <deepak.prabhakara@gmail.com>
2021-08-09 14:02:18 +0100

    lazy load iframe

--- src/pages/index.js
+++ src/pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
       </main>
 
       <p align="center">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/cRfAsvFS6vA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/cRfAsvFS6vA" loading="lazy" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
       </p>
     </Layout>
   );
