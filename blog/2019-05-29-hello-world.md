f4ed9cb4b5ae6595a40a57f7e26402be5d050543
Deepak Prabhakara <deepak.prabhakara@gmail.com>
2021-08-09 10:57:59 +0100

    updated to 2.0.0-alpha.74

--- blog/2019-05-29-hello-world.md
+++ blog/2019-05-29-hello-world.md
@@ -1,15 +1,14 @@
 ---
-id: hello-world
+slug: hello-world
 title: Hello
 author: Endilie Yacop Sucipto
-authorTitle: Maintainer of Docusaurus
-authorURL: https://github.com/endiliey
-authorImageURL: https://avatars1.githubusercontent.com/u/17883920?s=460&v=4
-authorTwitter: endiliey
+author_title: Maintainer of Docusaurus
+author_url: https://github.com/endiliey
+author_image_url: https://avatars1.githubusercontent.com/u/17883920?s=460&v=4
 tags: [hello, docusaurus]
 ---
 
-Welcome to this blog. This blog is created with [**Docusaurus 2 alpha**](https://v2.docusaurus.io/).
+Welcome to this blog. This blog is created with [**Docusaurus 2 alpha**](https://docusaurus.io/).
 
 <!--truncate-->
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,93 +1,38 @@
-/**
- * Copyright (c) 2017-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 import React from 'react';
-import classnames from 'classnames';
+import clsx from 'clsx';
 import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './styles.module.css';
+import styles from './index.module.css';
+import HomepageFeatures from '../components/HomepageFeatures';
 import Head from '@docusaurus/Head';
 
-const features = [
-  {
-    title: <>SAML</>,
-    imageUrl: 'fa-shield-alt',
-    description: (
-      <>
-        Add single sign on authentication into your product. Supports most
-        identity providers via SAML 2.0
-      </>
-    ),
-  },
-  {
-    title: <>Audit Logs</>,
-    imageUrl: 'fa-clipboard-list',
-    description: (
-      <>
-        Add audit logs into your product with ease. Allow your customers to send
-        security related events to their SIEM
-      </>
-    ),
-  },
-  {
-    title: <>Privacy Vault</>,
-    imageUrl: 'fa-key',
-    description: (
-      <>
-        Protect your customers sensitive data by safely encrypting it in a
-        Privacy vault. Add policies to govern access to the protected data
-      </>
-    ),
-  },
-  {
-    title: <>Role Based Access and Permissions</>,
-    imageUrl: 'fa-user-lock',
-    description: (
-      <>
-        Add roles, groups and permissions into your product using a flexible
-        schema. Query the schema at runtime in your product to enforce Role
-        Based Access Control
-      </>
-    ),
-  },
-  {
-    title: <>Directory Sync</>,
-    imageUrl: 'fa-sync',
-    description: (
-      <>
-        Add Directory Sync support into your product to provision and
-        de-provision users. Supports the SCIM protocol
-      </>
-    ),
-  },
-  {
-    title: <>(coming soon)</>,
-    imageUrl: 'fa-ellipsis-h',
-    description: (
-      <>
-        Admin Portal, Security Status Page, Compliance Monitoring, many more.
-        Contact us if you have any specific Enterprise features in mind.
-      </>
-    ),
-  },
-];
-
-function withIcon(icon) {
-  return `fas fa-lg fa-5x ${icon}`;
+function HomepageHeader() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="mailto:hello@boxyhq.com"
+          >
+            Contact Us
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
 }
 
-function Home() {
-  const context = useDocusaurusContext();
-  const { siteConfig = {} } = context;
+export default function Home() {
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`${siteConfig.title}`}
-      description="Boxy helps startups to enable enterprise features in any SaaS app with just a few lines of code"
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />"
     >
       <Head>
         <script
@@ -95,52 +40,11 @@ function Home() {
           crossorigin="anonymous"
         ></script>
       </Head>
-      <header className={classnames('hero hero--primary', styles.heroBanner)}>
-        <div className="container">
-          <h1 className="hero__title">{siteConfig.title}</h1>
-          <p className="hero__subtitle">{siteConfig.tagline}</p>
-          <div className={styles.buttons}>
-            <Link
-              className={classnames(
-                'button button--outline button--secondary button--lg',
-                styles.getStarted
-              )}
-              to="mailto:hello@boxyhq.com"
-            >
-              Get Started
-            </Link>
-          </div>
-        </div>
-      </header>
-      <main>
-        {features && features.length && (
-          <section className={styles.features}>
-            <div className="container">
-              <div className="row">
-                {features.map(({ imageUrl, title, description }, idx) => (
-                  <div
-                    key={idx}
-                    className={classnames('col col--4', styles.feature)}
-                  >
-                    {imageUrl && (
-                      <div className="text--center">
-                        <i class={withIcon(imageUrl)}></i>
 
-                        <br />
-                        <br />
-                      </div>
-                    )}
-                    <h3>{title}</h3>
-                    <p>{description}</p>
-                  </div>
-                ))}
-              </div>
-            </div>
-          </section>
-        )}
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
       </main>
     </Layout>
   );
 }
-
-export default Home;
