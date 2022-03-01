4cf57310594abfa64281957f08b4123d993ad8c5
Deepak Prabhakara <deepak@boxyhq.com>
2022-03-01 21:59:00 +0000

    added links to demo and mock saml

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,8 +12,8 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <h1 className="">{siteConfig.title}</h1>
+        <p className="">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
