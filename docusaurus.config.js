bb514a23e79a0fb59023c051ca2cb1610fc0884c
Deepak Prabhakara <deepak@boxyhq.com>
2022-07-11 19:24:52 +0100

    pin deps

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ module.exports = {
     announcementBar: {
       id: 'announcement-bar',
       content:
-        '<a target="_blank" rel="noopener noreferrer" href="https://github.com/boxyhq/jackson">⭐ Star us on GitHub</a>',
+        '<a target="_blank" rel="nofollow noopener noreferrer" href="https://github.com/boxyhq/jackson">⭐ Star us on GitHub</a>',
       isCloseable: false,
     },
     image: 'img/website-preview-image.png',
