b8e62f521776bb88836bc26b344a53fdfe83ff5c
Kiran <kiran@boxyhq.com>
2022-10-03 12:43:33 +0530

    Refactor the ProductSection into Card component

--- src/components/ProductsSection.js
+++ src/components/ProductsSection.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import Link from '@docusaurus/Link';
-import clsx from 'clsx';
 
-import SvgIcon from './SvgIcon';
+import Card from './Card';
 import SectionLayout from './SectionLayout';
 
 const products = [
@@ -59,55 +57,14 @@ const ProductsSection = () => {
     >
       <div className="row">
         {products.map(({ title, description, url, icon }, idx) => (
-          <div
-            className="col col--4"
-            style={{ marginBottom: '30px' }}
-            key={idx}
-          >
-            <div className="card-demo">
-              <div className="card">
-                <div className="card__header">
-                  <div className="card__image">
-                    <div className="avatar avatar--vertical">
-                      <div
-                        className="avatar__photo avatar__photo--lg"
-                        style={{ borderRadius: '0px' }}
-                      >
-                        <SvgIcon
-                          icon={icon}
-                          size={{ width: '64px', height: '64px' }}
-                        />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="card__body">
-                  <h4 className="text--center" style={{ fontSize: '20px' }}>
-                    {title}
-                  </h4>
-                  <p
-                    className="text--center"
-                    style={{ fontSize: '16px', minHeight: '5.5rem' }}
-                  >
-                    {description}
-                  </p>
-                </div>
-                <div className="card__footer">
-                  <Link
-                    to={url}
-                    className={clsx(
-                      'button button--primary button--block button--outline',
-                      {
-                        disabled: !url,
-                      }
-                    )}
-                  >
-                    {url ? 'Learn More' : 'Join Private Beta'}
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </div>
+          <Card
+            idx={idx}
+            title={title}
+            description={description}
+            icon={icon}
+            url={url}
+            btnText={url ? 'Learn More' : 'Join Private Beta'}
+          />
         ))}
       </div>
     </SectionLayout>
