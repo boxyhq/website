5a30da62d73703ba51e6ee5008915101a711a772
Kiran <kiran@boxyhq.com>
2022-11-30 21:20:55 +0530

    Update the icon

--- src/pages/pricing.js
+++ src/pages/pricing.js
@@ -13,48 +13,54 @@ const Pricing = () => {
         <h2 className="text--center" style={{ fontWeight: 'normal' }}>
           {description}
         </h2>
-        <div className="pricing__section__desktop">
-          <table className="pricing__table">
-            <thead>
-              <tr>
-                <th></th>
-                <th>Self-Hosted</th>
-                <th>Self-Hosted Premium</th>
-                <th>SaaS</th>
-                <th>Enterprise</th>
-              </tr>
-            </thead>
-            <tbody>
-              {pricingPlans.map((pricingPlan) => (
-                <tr key={pricingPlan.feature}>
-                  <td
-                    style={{
-                      textAlign: 'left',
-                      fontWeight: '500',
-                    }}
-                  >
-                    {pricingPlan.feature}
-                  </td>
-                  <td>{pricingPlan.tiers['self-hosted']}</td>
-                  <td>{pricingPlan.tiers['self-hosted-premium']}</td>
-                  <td>{pricingPlan.tiers['saas']}</td>
-                  <td>{pricingPlan.tiers['enterprise']}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-        <div className="pricing__section__mobile">
-          <PricingTableMobile title="Self-Hosted" tier="self-hosted" />
-          <PricingTableMobile
-            title="Self-Hosted Premium"
-            tier="self-hosted-premium"
-          />
-          <PricingTableMobile title="SaaS" tier="saas" />
-          <PricingTableMobile title="Enterprise" tier="enterprise" />
-        </div>
+        <PricingSectionDesktop />
+        <PricingSectionMobile />
       </div>
     </Layout>
+  );
+};
+
+const PricingSectionDesktop = () => {
+  return (
+    <div className="pricing__section__desktop">
+      <table className="pricing__table">
+        <thead>
+          <tr>
+            <th></th>
+            <th>Self-Hosted</th>
+            <th>Self-Hosted Premium</th>
+            <th>SaaS</th>
+            <th>Enterprise</th>
+          </tr>
+        </thead>
+        <tbody>
+          {pricingPlans.map((pricingPlan) => (
+            <tr key={pricingPlan.feature}>
+              <td
+                style={{
+                  textAlign: 'left',
+                  fontWeight: '500',
+                }}
+              >
+                {pricingPlan.feature}
+              </td>
+              <td>
+                <Text text={pricingPlan.tiers['self-hosted']} />
+              </td>
+              <td>
+                <Text text={pricingPlan.tiers['self-hosted-premium']} />
+              </td>
+              <td>
+                <Text text={pricingPlan.tiers['saas']} />
+              </td>
+              <td>
+                <Text text={pricingPlan.tiers['enterprise']} />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
@@ -75,12 +81,13 @@ const PricingTableMobile = ({ title, tier }) => {
               style={{
                 textAlign: 'left',
                 fontWeight: '400',
-                fontSize: '.875rem',
               }}
             >
               {pricingPlan.feature}
             </td>
-            <td style={{ textAlign: 'right' }}>{pricingPlan.tiers[tier]}</td>
+            <td style={{ textAlign: 'right' }}>
+              <Text text={pricingPlan.tiers[tier]} />
+            </td>
           </tr>
         ))}
       </tbody>
@@ -88,90 +95,154 @@ const PricingTableMobile = ({ title, tier }) => {
   );
 };
 
+const PricingSectionMobile = () => {
+  return (
+    <div className="pricing__section__mobile">
+      <PricingTableMobile title="Self-Hosted" tier="self-hosted" />
+      <PricingTableMobile
+        title="Self-Hosted Premium"
+        tier="self-hosted-premium"
+      />
+      <PricingTableMobile title="SaaS" tier="saas" />
+      <PricingTableMobile title="Enterprise" tier="enterprise" />
+    </div>
+  );
+};
+
+const IconYes = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="#25c2a0"
+      width="24"
+      height="24"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+      />
+    </svg>
+  );
+};
+
+const IconNo = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="#F87272"
+      width="24"
+      height="24"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+      />
+    </svg>
+  );
+};
+
 const ContactUs = () => {
   return <a href="mailto:deepak@boxyhq.com">Contact us</a>;
+};
+
+const Text = ({ text }) => {
+  const iconsMap = {
+    yes: <IconYes />,
+    no: <IconNo />,
+    contactUs: <ContactUs />,
+  };
+
+  return iconsMap[text] || <span>{text}</span>;
 };
 
 const pricingPlans = [
   {
     feature: 'Premium features',
     tiers: {
-      'self-hosted': '❌',
-      'self-hosted-premium': '✅',
-      saas: '✅',
-      enterprise: '✅',
+      'self-hosted': 'yes',
+      'self-hosted-premium': 'yes',
+      saas: 'yes',
+      enterprise: 'yes',
     },
   },
   {
     feature: 'Admin Portal',
     tiers: {
-      'self-hosted': '✅',
-      'self-hosted-premium': '✅',
-      saas: '✅',
-      enterprise: '✅',
+      'self-hosted': 'yes',
+      'self-hosted-premium': 'yes',
+      saas: 'yes',
+      enterprise: 'yes',
     },
   },
   {
     feature: 'Discord Support',
     tiers: {
-      'self-hosted': '✅',
-      'self-hosted-premium': '✅',
-      saas: '✅',
-      enterprise: '✅',
+      'self-hosted': 'yes',
+      'self-hosted-premium': 'yes',
+      saas: 'yes',
+      enterprise: 'yes',
     },
   },
   {
     feature: 'Email and chat Support',
     tiers: {
-      'self-hosted': '❌',
-      'self-hosted-premium': '✅',
-      saas: '✅',
-      enterprise: '✅',
+      'self-hosted': 'no',
+      'self-hosted-premium': 'yes',
+      saas: 'yes',
+      enterprise: 'yes',
     },
   },
   {
     feature: 'Phone support',
     tiers: {
-      'self-hosted': '❌',
-      'self-hosted-premium': '✅',
-      saas: '✅',
-      enterprise: '✅',
+      'self-hosted': 'no',
+      'self-hosted-premium': 'yes',
+      saas: 'yes',
+      enterprise: 'yes',
     },
   },
   {
     feature: 'Custom deployment',
     tiers: {
-      'self-hosted': '❌',
-      'self-hosted-premium': '✅',
-      saas: '❌',
-      enterprise: '✅',
+      'self-hosted': 'no',
+      'self-hosted-premium': 'yes',
+      saas: 'no',
+      enterprise: 'yes',
     },
   },
   {
     feature: 'Custom SLAs',
     tiers: {
-      'self-hosted': '❌',
-      'self-hosted-premium': '❌',
-      saas: '❌',
-      enterprise: '✅',
+      'self-hosted': 'no',
+      'self-hosted-premium': 'no',
+      saas: 'no',
+      enterprise: 'yes',
     },
   },
   {
     feature: 'Dedicated infrastructure capacity',
     tiers: {
-      'self-hosted': '❌',
-      'self-hosted-premium': '❌',
-      saas: '❌',
-      enterprise: '✅',
+      'self-hosted': 'no',
+      'self-hosted-premium': 'no',
+      saas: 'no',
+      enterprise: 'yes',
     },
   },
   {
     feature: 'Custom terms & contract',
     tiers: {
-      'self-hosted': '❌',
-      'self-hosted-premium': '❌',
-      saas: '❌',
-      enterprise: '✅',
+      'self-hosted': 'no',
+      'self-hosted-premium': 'no',
+      saas: 'no',
+      enterprise: 'yes',
     },
   },
   {
@@ -180,25 +251,25 @@ const pricingPlans = [
       'self-hosted': 'Free forever',
       'self-hosted-premium': '$49 per SSO or Directory Sync connection',
       saas: '$49 per SSO or Directory Sync connection',
-      enterprise: <ContactUs />,
+      enterprise: 'contactUs',
     },
   },
   {
     feature: 'Audit logs',
     tiers: {
       'self-hosted': 'Free forever',
-      'self-hosted-premium': <ContactUs />,
-      saas: <ContactUs />,
-      enterprise: <ContactUs />,
+      'self-hosted-premium': 'contactUs',
+      saas: 'contactUs',
+      enterprise: 'contactUs',
     },
   },
   {
     feature: 'Privacy Vault',
     tiers: {
       'self-hosted': 'Free forever',
-      'self-hosted-premium': <ContactUs />,
-      saas: <ContactUs />,
-      enterprise: <ContactUs />,
+      'self-hosted-premium': 'contactUs',
+      saas: 'contactUs',
+      enterprise: 'contactUs',
     },
   },
 ];
