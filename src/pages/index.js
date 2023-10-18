1484e6b789c86cc842887119de99f2694c0291fb
Swastik Patel <swastikpatel29@gmail.com>
2023-10-18 09:55:18 +0530

    Fix Button responsiveness (#292)
    * button responsiveness fixed
    
    * final commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,6 @@ const description = 'Security Building Blocks for Developers';
 const IndexPage = () => {
   return (
     <Layout title={title} description={description}>
-     
       <HeroSection
         title="Security Building Blocks for Developers"
         description="Reduce Time to Market without sacrificing your security posture! BoxyHQ’s suite of APIs for security and privacy helps engineering teams build and ship compliant cloud applications faster."
@@ -44,8 +43,6 @@ const IndexPage = () => {
           },
         ]}
       />
-
-     
 
       <CustomersSection />
       <ProductsSection />
@@ -80,7 +77,14 @@ const IndexPage = () => {
                     </div>
                     <div className="card__footer">
                       <Link
-                        className={clsx('button button--primary button--block')}
+                        className={clsx(
+                          'button button--primary button--block '
+                        )}
+                        style={{
+                          whiteSpace: 'normal',
+                          overflow: 'hidden',
+                          width: '100%',
+                        }}
                         href="https://meetings.hubspot.com/deepakprab/demo?__hstc=213510283.9a563789bb583fca1e9fb20a629c5c94.1651597904536.1664893584285.1664895920328.169&__hssc=213510283.1.1664895920328&__hsfp=498882655"
                       >
                         Book a free developer-security session
