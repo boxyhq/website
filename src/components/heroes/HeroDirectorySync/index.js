561b3883779414b5d3abb546997a173f693d8931
Schalk Neethling <schalk@mechanical.ink>
2024-01-25 20:24:05 +0200

    directory sync landing and cleanup

---
+++ src/components/heroes/HeroDirectorySync/index.js
@@ -0,0 +1,40 @@
+import BaseHero from '../BaseHero';
+
+const HeroDirectorySync = () => {
+  return (
+    <BaseHero
+      pictureSrc="/images/heroes/directory-sync-hero"
+      sectionId="directory-sync"
+      title="BoxyHQ <br />Directory Sync"
+    >
+      <p>
+        Discover the power of streamlined user and group provisioning with
+        BoxyHQ's Directory Sync. Our solution fully supports the SCIM 2.0
+        protocol, ensuring seamless compatibility with the majority of major
+        Identity Providers. Elevate your system's efficiency and compliance with
+        a robust directory synchronization tool designed for the modern
+        enterprise.
+      </p>
+      <ul className="reset-list button-container">
+        <li>
+          <a
+            className="button button-secondary with-icon base-icon-pseudo icon-laptop"
+            href="https://cal.com/deepak-boxyhq/demo"
+          >
+            Book A Demo
+          </a>
+        </li>
+        <li>
+          <a
+            className="button button-primary with-icon base-icon-pseudo icon-code-slash"
+            href="https://app.eu.boxyhq.com/auth/join"
+          >
+            Sign Up Today
+          </a>
+        </li>
+      </ul>
+    </BaseHero>
+  );
+};
+
+export default HeroDirectorySync;
