252303d634088ce4cf61c6dd5a390b945b283221
Schalk Neethling <schalk@mechanical.ink>
2024-04-04 18:03:44 +0200

    delete customer-facing from hero copy (#413)

--- src/components/heroes/HeroIdentityFederation/index.js
+++ src/components/heroes/HeroIdentityFederation/index.js
@@ -14,9 +14,9 @@ const HeroIdentityFederation = () => {
         As businesses scale, the complexity of managing customer or employee
         identities across multiple applications intensifies. BoxyHQ's Identity
         Federation Broker addresses this challenge by streamlining user identity
-        management across all customer-facing applications, irrespective of
-        their diverse configurations or Identity Provider (IdP) protocols, such
-        as SAML, OIDC, or various IdPs.
+        management across all applications, irrespective of their diverse
+        configurations or Identity Provider (IdP) protocols, such as SAML, OIDC,
+        or various IdPs.
       </p>
       <ul className="reset-list button-container">
         <li>
